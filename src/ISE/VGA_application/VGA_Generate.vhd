03562f508dc720bf67af0a71d510b1fae09b8661
FSHexa <Fedorenko.S.S@yandex.ru>
2017-07-25 00:17:49 +0300

    Добавил автомат из прошлой версии файла

--- src/ISE/VGA_application/VGA_Generate.vhd
+++ src/ISE/VGA_application/VGA_Generate.vhd
@@ -33,9 +33,9 @@ Port(   CLK                         : in    std_logic;
         
         VGA_VSYNC                   : out   std_logic;
         VGA_HSYNC                   : out   std_logic;
-        VGA_RED                     : out   std_logic_vector(4 downto 0);
-        VGA_GREEN                   : out   std_logic_vector(5 downto 0);
-        VGA_BLUE                    : out   std_logic_vector(4 downto 0));
+        VGA_RED                     : out   std_logic_vector(7 downto 0);
+        VGA_GREEN                   : out   std_logic_vector(7 downto 0);
+        VGA_BLUE                    : out   std_logic_vector(7 downto 0));
 end VGA_Generate;
 
 architecture Behavioral of VGA_Generate is
@@ -75,6 +75,8 @@ signal h_state                      : t_state   := SYNC;
 
 signal c_vga_vsync                  : integer   := 0;
 signal c_vga_hsync                  : integer   := 0;
+
+signal end_string                   : std_logic := '0';
 -- ============================================================================ --
 --                                   Programm                                   --
 -- ============================================================================ --
@@ -95,6 +97,11 @@ port map(   clka                    => VGA_BRAM_clk,
 -- ---------------------------------------------------------------------------- --
 --                                   Programm                                   --
 -- ---------------------------------------------------------------------------- --
+VGA_VSYNC   <= s_vga_vsync;
+VGA_HSYNC   <= s_vga_hsync;
+VGA_RED     <= s_vga_red;
+VGA_GREEN   <= s_vga_green;
+VGA_BLUE    <= s_vga_blue;
 
 -- ==================================================== --
 -- Process info                                         --
@@ -102,34 +109,145 @@ port map(   clka                    => VGA_BRAM_clk,
 process(VGA_BRAM_clk)
 begin
     if(rising_edge(VGA_BRAM_clk)) then
-    
-        case v_state is
+        if(end_string = '1') then
+            case v_state is
+                ----------------------
+                when SYNC           =>
+                ----------------------
+                    -- =================================================================== --
+                        if(c_vga_vsync = 5) then
+                            c_vga_vsync <= 0;
+                            v_state     <= SYNC_DEL;
+                            s_vga_vsync <= '1';
+                        else
+                            c_vga_vsync <= c_vga_vsync + 1;
+                        end if;
+                    -- =================================================================== --
+                ----------------------
+                when SYNC_DEL       =>
+                ----------------------
+                    -- =================================================================== --
+                        if(c_vga_vsync = 28) then
+                            c_vga_vsync <= 0;
+                            v_state     <= DATA;
+                        else
+                            c_vga_vsync <= c_vga_vsync + 1;
+                        end if;
+                    -- =================================================================== --
+                ----------------------
+                when DATA           =>
+                ----------------------
+                    -- =================================================================== --
+                        if(c_vga_vsync = 767) then
+                            c_vga_vsync <= 0;
+                            v_state     <= DATA_DEL;
+                        else
+                            c_vga_vsync <= c_vga_vsync + 1;
+                        end if;
+                    -- =================================================================== --
+                ----------------------
+                when DATA_DEL       =>
+                ----------------------
+                    -- =================================================================== --
+                        if(c_vga_vsync = 2) then
+                            c_vga_vsync <= 0;
+                            v_state     <= SYNC;
+                            s_vga_vsync <= '0';
+                        else
+                            c_vga_vsync <= c_vga_vsync + 1;
+                        end if;
+                    -- =================================================================== --
+            end case;
+        end if;
+        
+        case h_state is
             ----------------------
             when SYNC           =>
             ----------------------
                 -- =================================================================== --
+                    s_vga_red   <= X"00";
+                    s_vga_green <= X"00";
+                    s_vga_blue  <= X"00";
                     
+                    if(c_vga_hsync = 136) then
+                        c_vga_hsync <= 0;
+                        h_state     <= SYNC_DEL;
+                        s_vga_hsync <= '1';
+                    else
+                        c_vga_hsync <= c_vga_hsync + 1;
+                    end if;
                 -- =================================================================== --
             ----------------------
             when SYNC_DEL       =>
             ----------------------
                 -- =================================================================== --
+                    s_vga_red   <= X"00";
+                    s_vga_green <= X"00";
+                    s_vga_blue  <= X"00";
                     
+                    if(c_vga_hsync = 162) then
+                        c_vga_hsync <= 0;
+                        h_state     <= DATA;
+                        s_vga_hsync <= '1';
+                    else
+                        c_vga_hsync <= c_vga_hsync + 1;
+                    end if;
                 -- =================================================================== --
             ----------------------
             when DATA           =>
             ----------------------
                 -- =================================================================== --
+                    if(c_vga_hsync = 1023) then
+                        c_vga_hsync <= 0;
+                        h_state     <= DATA_DEL;
+                        s_vga_hsync <= '1';
+                    else
+                        c_vga_hsync <= c_vga_hsync + 1;
+                    end if;
                     
+                    -- if(v_state /= DATA) then                                     -- править
+                        -- s_vga_red   <= X"00";
+                        -- s_vga_green <= X"00";
+                        -- s_vga_blue  <= X"00";
+                    -- else
+                        -- if(write_data = '0') then
+                            -- s_vga_red   <= data_vga_1(c_vga_hsync)(23 downto 16);
+                            -- s_vga_green <= data_vga_1(c_vga_hsync)(15 downto 8);
+                            -- s_vga_blue  <= data_vga_1(c_vga_hsync)(7 downto 0);
+                        -- else
+                            -- s_vga_red   <= data_vga_2(c_vga_hsync)(23 downto 16);
+                            -- s_vga_green <= data_vga_2(c_vga_hsync)(15 downto 8);
+                            -- s_vga_blue  <= data_vga_2(c_vga_hsync)(7 downto 0);
+                        -- end if;
+                    -- end if;
                 -- =================================================================== --
             ----------------------
             when DATA_DEL       =>
             ----------------------
                 -- =================================================================== --
+                    s_vga_red   <= X"00";
+                    s_vga_green <= X"00";
+                    s_vga_blue  <= X"00";
                     
+                    if(c_vga_hsync = 25) then
+                        c_vga_hsync <= 0;
+                        end_string  <= '0';
+                        h_state     <= SYNC;
+                        s_vga_hsync <= '0';
+                        if(c_write_data = 7) then
+                            write_data  <= not(write_data);                     -- править
+                            c_write_data<= 0;
+                        else
+                            c_write_data    <= c_write_data + 1;
+                        end if;
+                    else
+                        c_vga_hsync <= c_vga_hsync + 1;
+                        if(c_vga_hsync = 24) then
+                            end_string  <= '1';
+                        end if;
+                    end if;
                 -- =================================================================== --
         end case;
-        
     end if;
 end process;
     
